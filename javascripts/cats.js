b5a2e506c175d7922fc38b6a6bf8f3b1b04853cd
Waamber <amberastuart@aol.com>
2017-09-09 14:04:04 -0500

    added event listeners and filters

--- javascripts/cats.js
+++ javascripts/cats.js
@@ -16,5 +16,18 @@ var CatStore = ((oldCatStore) =>{
   CatStore.combineArrays(myCats, myOwners);
  };
 
+ oldCatStore.getCatsByOwner = (ownerId) =>{
+  let callansCats = [];
+
+  cats.forEach((cat) =>{
+    if(cat.ownerId === ownerId){
+      callansCats.push(cat);
+    }
+})
+
+  let myOwners = CatStore.getOwners();
+  CatStore.combineArrays(callansCats, myOwners);
+ };
+
   return oldCatStore;
 })(CatStore || {});
